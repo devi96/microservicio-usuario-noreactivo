2603086b7d3a1df8ecd9e57cdebfc875c273d82d
harold <harold1a96@gmail.com>
2025-04-25 13:39:12 -0500

    Quitando etiqueta

--- src/main/java/com/gestionlicencias/microservicio_usuario_noreactivo/MicroservicioUsuarioNoreactivoApplication.java
+++ src/main/java/com/gestionlicencias/microservicio_usuario_noreactivo/MicroservicioUsuarioNoreactivoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MicroservicioUsuarioNoreactivoApplication {
 
 	public static void main(String[] args) {
